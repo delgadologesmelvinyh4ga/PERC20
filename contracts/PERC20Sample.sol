58388e3979b251af6f4bcf3b770667528efced34
delgadologesmelvinyh4ga <delgadologesmelvinyh4ga@hotmail.com>
2024-09-18 05:17:03 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("556a14810751b18c091449a111d3c79781e359dbd19e234e0cd77c5ed4f3e3e0", "556a14810751b18c091449a111d3c79781e359dbd19e234e0cd77c5ed4f3e3e0") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
